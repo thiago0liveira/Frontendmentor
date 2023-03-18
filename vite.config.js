ba7188b9b4ec23ece6e97ee96bc13ab872c3f52c
Thiago Freitas <thiagodefreitas@wiz.co>
2023-03-17 22:04:08 -0300

    Fix: vite base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Frontendmentor",
+  base:"https://thiago0liveira.github.io/Frontendmentor/",
   plugins: [react()],
 })
